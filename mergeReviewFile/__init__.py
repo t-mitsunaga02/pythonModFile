aa0e5db3e0edc024116b666ab4606ee7bef5ef39
t-mitsunaga02 <t-mitsunaga@analytics-jp.com>
2023-12-11 20:51:56 +0900

    modify commit

--- mergeReviewFile/__init__.py
+++ mergeReviewFile/__init__.py
@@ -1,24 +1,47 @@
 import logging
 
 import azure.functions as func
-
+from azure.storage.blob import BlobServiceClient
+import pandas as pd
+import os
+import io
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    # 1.レビューデータの読み込み
+    ## BLOBへの接続
+    connect_str = os.getenv("AzureWebJobsStorage")
+    ## Create a blob client using the local file name as the name for the blob
+    blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+    ## BLOB入出力先の設定
+    container_name = "scrapefile"
+    blob_name_in = "dashboard_motive/raw/scrape/"
+    blob_name_out = "dashboard_motive/modify/merge/motive_modify_mergefile.csv"
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    ## レビューデータ取得
+    container_client = blob_service_client.get_container_client(container_name)
+    blob_list = container_client.list_blobs(name_starts_with=blob_name_in)
+    csv_files = [blob.name for blob in blob_list if blob.name.endswith('.csv')]
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    # CSVファイルを読み込んでマージ
+    df_list = []
+    for file_name in csv_files:
+        blob_client = blob_service_client.get_blob_client(container=container_name, blob=file_name)
+        with blob_client.download_blob() as download_stream:
+            df = pd.read_csv(download_stream)
+            df_list.append(df)
+
+    merged_df = pd.concat(df_list)
+
+    # 2.マージファイルを出力
+    # マージしたDataFrameを新しいファイルとして保存
+    output_blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name_out)
+    output_blob_client.upload_blob(merged_df.to_csv(index=False, encoding='utf_8'), blob_type="BlockBlob", overwrite=True)
+
+    # 3.元ファイルの削除
+    for file_delete in csv_files:
+        blob_client_delete = blob_service_client.get_blob_client(container=container_name, blob=file_delete)
+        blob_client_delete.delete_blob()
+
+    return func.HttpResponse(
+                status_code=200
+    )
