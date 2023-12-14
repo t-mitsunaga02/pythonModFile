895d53bfc15715d58acf3574a07bea4db6aa2fcd
t-mitsunaga02 <t-mitsunaga@analytics-jp.com>
2023-12-14 12:15:57 +0900

    modify commit

--- modifyReviewFile/__init__.py
+++ modifyReviewFile/__init__.py
@@ -7,7 +7,7 @@ from openai import AzureOpenAI
 def main(req: func.HttpRequest) -> func.HttpResponse:
     client = AzureOpenAI(
         api_key=os.getenv("AZURE_OPENAI_KEY"),  
-        api_version="1106-Preview",
+        api_version="gpt-4",
         azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
         )
         
